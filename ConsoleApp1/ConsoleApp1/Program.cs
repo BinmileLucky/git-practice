499483ee98a42f4a5c8ebe98d764e66a38374200
BinmileLucky <lucky@binmile.com>
2023-08-03 12:27:27 +0530

    console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,162 @@
+﻿//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//       try
+//        {
+//            int[] myarr = { 1, 2, 3, 4 };
+//            Console.WriteLine(myarr[10]);
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine(ex.ToString());
+//            Console.WriteLine(ex.Message);
+//            Console.WriteLine("NOPE");
+//        }
+//    }
+//}
+
+
+
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        string month = Console.ReadLine();
+
+//        switch (month)
+//        {
+//            case "january":
+//                {
+//                    Console.WriteLine(1);
+//                    break;
+//                }
+//            case "feburary":
+//                {
+//                    Console.WriteLine(2);
+//                    break;
+//                }
+//            case "march":
+//                {
+//                    Console.WriteLine(3);
+//                    break;
+//                }
+//            case "april":
+//                {
+//                    Console.WriteLine(4);
+//                    break;
+//                }
+//            case "may":
+//                {
+//                    Console.WriteLine(5);
+//                    break;
+//                }
+//            case "june":
+//                {
+//                    Console.WriteLine(6);
+//                    break;
+//                }
+//            case "july":
+//                {
+//                    Console.WriteLine(7);
+//                    break;
+//                }
+//            case "august":
+//                {
+//                    Console.WriteLine(8);
+//                    break;
+//                }
+//            case "september":
+//                {
+//                    Console.WriteLine(9);
+//                    break;
+//                }
+//            case "october":
+//                {
+//                    Console.WriteLine(10);
+//                    break;
+//                }
+//            case "november":
+//                {
+//                    Console.WriteLine(11);
+//                    break;
+//                }
+//            case "december":
+//                {
+//                    Console.WriteLine(12);
+//                    break;
+
+//                }
+//            default:
+//                {
+//                    Console.WriteLine("invalid month or try in lower case");
+//                    break;
+//                }
+
+
+
+
+//        }
+//    }
+//}
+
+
+
+
+using System.Dynamic;
+using ConsoleApp1;
+
+internal class program
+{
+    private static void Main(string[] args)
+    {
+        //int[,] arr = new int[3, 3];
+        //int k = 0;
+
+        //for(int i=0; i<arr.GetLength(0); i++)
+        //{
+        //    for(int j=0; j<arr.GetLength(1); j++)
+        //    {
+        //        arr[i,j] = k;
+        //        k++;
+        //    }
+        //}
+
+        //int left = 0;
+        //int right = 0;
+
+        //Foor Loop Demo
+
+        
+
+        string decide=Console.ReadLine();
+
+        switch(decide)
+        {
+            case "for":
+                {
+                    ForLoopDemo forLoopDemo = new ForLoopDemo();
+
+                    var _result = forLoopDemo.GetResult();
+
+                    Console.WriteLine(_result);
+
+                    break;
+                }
+
+            case "while":
+                {
+                    WhileLoopDemo whileLoopDemo = new WhileLoopDemo();
+                    int result =whileLoopDemo.whilefunction();
+                    Console.WriteLine(result);
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("function not defined");
+                    break;
+                }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/WhileLoopDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class WhileLoopDemo
+    {
+
+      public int whilefunction()
+        {
+            int sm = 0;
+            int x = 10, i = 0;
+            while (i < x)
+            {
+                sm+=i;
+                i++;
+            }
+            return sm;
+
+        }
+    }
+}
