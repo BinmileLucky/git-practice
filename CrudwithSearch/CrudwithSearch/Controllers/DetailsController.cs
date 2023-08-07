e7a85ca7ed21989c914cd92e19d07f884e3cfcc2
BinmileLucky <lucky@binmile.com>
2023-08-07 10:49:40 +0530

    implement search functionality with pagination in Registration form

---
+++ CrudwithSearch/CrudwithSearch/Controllers/DetailsController.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using CrudwithSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PagedList;
+//using Model PagedList<CrudwithSearch.Models.EmployeeDetail>
+
+namespace CrudwithSearch.Controllers
+{
+    public class DetailsController : Controller
+    {
+        RegistartionDetailsContext db = new RegistartionDetailsContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddData(EmployeeDetail e)
+        {
+             db.EmployeeDetails.Add(e);
+            int a = db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+     
+        public IActionResult GetData(int PageNumber=1)
+        {
+
+                  
+                var data = db.EmployeeDetails.Skip((PageNumber-1)*4).Take(4).ToList();
+          ViewBag.TotalPages = db.EmployeeDetails.Count() /3;
+            ////filter
+            //var pagedEmp = new PagedList<EmployeeDetail>(data, pageno, pagesize);
+
+                return View(data);
+            
+        }
+
+        [HttpPost]
+        public ActionResult SearchData(string name)
+        {
+
+
+            var sd = db.EmployeeDetails.Where(model => model.EmpName == name).ToList();
+                return View(sd);
+            
+            
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            var row = db.EmployeeDetails.Where(model => model.Id == id).FirstOrDefault();
+
+
+            return View(row);
+
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+
+            var row = db.EmployeeDetails.Where(model => model.Id == id).FirstOrDefault();
+
+            db.Entry(row).State = EntityState.Deleted;
+            int a = db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+
+    }
+}
---
+++ CrudwithSearch/CrudwithSearch/Models/EmployeeDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudwithSearch.Models;
+
+public partial class EmployeeDetail
+{
+    public int Id { get; set; }
+
+    public string EmpName { get; set; } = null!;
+
+    public string EmpDepartment { get; set; } = null!;
+
+    public string EmpPhone { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+}
